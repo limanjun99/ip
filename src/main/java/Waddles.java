f8b09ddc34f04b71aab66d826edbee18eaba21e0
An Jun <30954848+limanjun99@users.noreply.github.com>
2025-08-17 20:45:16 +0800

    Echo user input
    Add simple echoing functionality to Waddles.
    * Make Waddles echo user input until user enters "bye".
    * Make Waddles' output left-padded for readability.

--- src/main/java/Waddles.java
+++ src/main/java/Waddles.java
@@ -1,10 +1,28 @@
+import java.util.Scanner;
+
 public class Waddles {
     public static void main(String[] args) {
         printGreeting();
+        runInputLoop();
         printFarewell();
     }
 
     private static final String NAME = "Waddles";
+
+    private static void runInputLoop() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                break;
+            }
+            handleInput(input);
+        }
+    }
+
+    private static void handleInput(String input) {
+        printMessage(input);
+    }
 
     private static void printGreeting() {
         String greeting = String.format("Hello! I'm %s.\n" + "What can I do for you?", NAME);
@@ -17,13 +35,16 @@ public class Waddles {
     }
 
     /**
-     * Wraps the message in horizontal lines before printing it to stdout.
+     * Formats the message nicely and prints it to stdout.
      * Prefer this method for printing to keep output consistent.
      */
     private static void printMessage(String message) {
-        final String HORIZONTAL_LINE = "-".repeat(80);
-        System.out.println(HORIZONTAL_LINE);
-        System.out.println(message);
-        System.out.println(HORIZONTAL_LINE);
+        final String leftPadding = " ".repeat(4);
+        final String horizontalLine = "-".repeat(80);
+        System.out.println(leftPadding + horizontalLine);
+        for (String line : message.split("\n")) {
+            System.out.println(leftPadding + line);
+        }
+        System.out.println(leftPadding + horizontalLine);
     }
 }
