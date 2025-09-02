f463eb2d881584de55b0ff554d40a6000ea423ca
An Jun <30954848+limanjun99@users.noreply.github.com>
2025-09-02 18:34:11 +0800

    Add GUI to Waddles with JavaFX

--- src/main/java/waddles/Waddles.java
+++ src/main/java/waddles/Waddles.java
@@ -1,7 +1,6 @@
 package waddles;
 
 import java.time.LocalDateTime;
-import java.util.Scanner;
 
 import waddles.task.Deadline;
 import waddles.task.Event;
@@ -26,133 +25,139 @@ public class Waddles {
         ui = new WaddlesUi();
     }
 
-    public static void main(String[] args) {
-        Waddles waddles = new Waddles();
-        waddles.run();
-    }
-
     /**
-     * Starts the Waddles chatbot.
-     * Listens to user input from stdin and responds appropriately to stdout.
+     * Processes a single user command and returns a response to it.
      */
-    public void run() {
-        ui.printGreeting();
-
-        Scanner scanner = new Scanner(System.in);
-        boolean isDone = false;
-        while (!isDone) {
-            String input = scanner.nextLine();
-            try {
-                isDone = handleInput(input);
-                saveFile.save(tasks);
-            } catch (WaddlesException e) {
-                ui.printError(e);
-            }
+    public WaddlesResult getResponse(String input) {
+        try {
+            WaddlesResult result = handleInput(input);
+            saveFile.save(tasks);
+            return result;
+        } catch (WaddlesException e) {
+            return WaddlesResult.makeError(ui.makeErrorMessage(e));
         }
-
-        ui.printFarewell();
     }
 
     /**
      * Handles a single line of input from the user.
-     *
-     * @return Whether this input should make Waddles exit.
      */
-    private boolean handleInput(String input) throws WaddlesException {
+    @SuppressWarnings("checkstyle:Indentation")
+    private WaddlesResult handleInput(String input) throws WaddlesException {
         Parser parser = new Parser(input);
         Command command = parser.getCommand();
         switch (command) {
-        case BYE -> {
-            return true;
+        case BYE:
+            return handleBye();
+        case LIST:
+            return handleList();
+        case MARK:
+            return handleMark(parser);
+        case UNMARK:
+            return handleUnmark(parser);
+        case TODO:
+            return handleAddTodo(parser);
+        case DEADLINE:
+            return handleAddDeadline(parser);
+        case EVENT:
+            return handleAddEvent(parser);
+        case DELETE:
+            return handleDelete(parser);
+        case FIND:
+            return handleFind(parser);
+        default:
+            throw new WaddlesException.InvalidCommand(input);
         }
-        case LIST -> ui.printTasks(tasks);
-        case MARK -> handleMark(parser);
-        case UNMARK -> handleUnmark(parser);
-        case TODO -> handleAddTodo(parser);
-        case DEADLINE -> handleAddDeadline(parser);
-        case EVENT -> handleAddEvent(parser);
-        case DELETE -> handleDelete(parser);
-        case FIND -> handleFind(parser);
-        default -> throw new WaddlesException.InvalidCommand(input);
-        }
-        return false;
+    }
+
+    /**
+     * Terminates Waddles.
+     */
+    private WaddlesResult handleBye() {
+        return WaddlesResult.makeEnd(ui.makeFarewellMessage());
+    }
+
+    /**
+     * Lists all current tasks.
+     */
+    private WaddlesResult handleList() {
+        return WaddlesResult.makeSuccess(ui.makeTasksMessage(tasks));
     }
 
     /**
      * Adds a new Todo task.
      * The user input should have the format {@code todo <task description>}.
      */
-    private void handleAddTodo(Parser parser) throws WaddlesException {
+    private WaddlesResult handleAddTodo(Parser parser) throws WaddlesException {
         String description = parser.readArgument("task description", "");
         Todo todo = new Todo(description, false);
         tasks.add(todo);
-        ui.printAddedTask(tasks, todo);
+        return WaddlesResult.makeSuccess(ui.makeAddedTaskMessage(tasks, todo));
     }
 
     /**
      * Adds a new Deadline task.
      * The user input should have the format {@code deadline <task description> /by <deadline>}.
      */
-    private void handleAddDeadline(Parser parser) throws WaddlesException {
+    private WaddlesResult handleAddDeadline(Parser parser) throws WaddlesException {
         String description = parser.readArgument("task description", " /by ");
         LocalDateTime by = parser.readDateTimeArgument("deadline (/by)", "");
         Deadline deadline = new Deadline(description, false, by);
         tasks.add(deadline);
-        ui.printAddedTask(tasks, deadline);
+        return WaddlesResult.makeSuccess(ui.makeAddedTaskMessage(tasks, deadline));
     }
 
     /**
      * Adds a new Event task.
      * The user input should have the format {@code event <task description> /from <start> /to <end>}.
      */
-    private void handleAddEvent(Parser parser) throws WaddlesException {
+    private WaddlesResult handleAddEvent(Parser parser) throws WaddlesException {
         String description = parser.readArgument("task description", " /from ");
         LocalDateTime from = parser.readDateTimeArgument("from (/from)", " /to ");
         LocalDateTime to = parser.readDateTimeArgument("to (/to)", "");
         Event event = new Event(description, false, from, to);
         tasks.add(event);
-        ui.printAddedTask(tasks, event);
+        return WaddlesResult.makeSuccess(ui.makeAddedTaskMessage(tasks, event));
     }
 
     /**
      * Marks a task as done.
      * The user input should have the format {@code mark <task_index>}.
      */
-    private void handleMark(Parser parser) throws WaddlesException {
+    private WaddlesResult handleMark(Parser parser) throws WaddlesException {
         int taskIndex = parser.readIntegerArgument("task index", "");
         Task task = tasks.get(taskIndex);
         task.setDone(true);
-        ui.printMarkedTask(task);
+        return WaddlesResult.makeSuccess(ui.makeMarkedTaskMessage(task));
     }
 
     /**
      * Unmarks a done task (i.e. sets it to be undone).
      * The user input should have the format {@code unmark <task_index>}.
      */
-    private void handleUnmark(Parser parser) throws WaddlesException {
+    private WaddlesResult handleUnmark(Parser parser) throws WaddlesException {
         int taskIndex = parser.readIntegerArgument("task index", "");
         Task task = tasks.get(taskIndex);
         task.setDone(false);
-        ui.printUnmarkedTask(task);
+        return WaddlesResult.makeSuccess(ui.makeUnmarkedTaskMessage(task));
     }
 
     /**
      * Deletes a task.
      * The user input should have the format {@code delete <task_index>}.
      */
-    private void handleDelete(Parser parser) throws WaddlesException {
+    private WaddlesResult handleDelete(Parser parser) throws WaddlesException {
         int taskIndex = parser.readIntegerArgument("task index", "");
         Task task = tasks.remove(taskIndex);
-        ui.printDeletedTask(tasks, task);
+        return WaddlesResult.makeSuccess(ui.makeDeletedTaskMessage(tasks, task));
     }
 
     /**
      * Finds tasks containing a keyword.
      * The user input should have the format {@code find <keyword>}.
      */
-    private void handleFind(Parser parser) throws WaddlesException {
+    private WaddlesResult handleFind(Parser parser) throws WaddlesException {
         String keyword = parser.readArgument("keyword", "");
         Tasks filteredTasks = tasks.find(keyword);
-        ui.printTasks(filteredTasks);
+        return WaddlesResult.makeSuccess(ui.makeTasksMessage(filteredTasks));
     }
 }
--- src/main/java/waddles/WaddlesUi.java
+++ src/main/java/waddles/WaddlesUi.java
@@ -4,96 +4,81 @@ import waddles.task.Task;
 import waddles.task.Tasks;
 
 /**
- * Handles printing information to the command line.
+ * Responsible for the output format for various commands.
  */
 public class WaddlesUi {
     private static final String NAME = "Waddles";
 
     /**
-     * Prints a greeting to the user.
+     * Returns a greeting.
      */
-    public void printGreeting() {
-        String greeting = String.format("Hello! I'm %s.\n" + "What can I do for you?", NAME);
-        printMessage(greeting);
+    public String makeGreetingMessage() {
+        return String.format("Hello! I'm %s.\n" + "What can I do for you?", NAME);
     }
 
     /**
-     * Prints a farewell message.
+     * Returns a farewell message.
      */
-    public void printFarewell() {
-        String farewell = "Bye. Hope to see you again soon!";
-        printMessage(farewell);
+    public String makeFarewellMessage() {
+        return "Bye. Hope to see you again soon!";
     }
 
     /**
-     * Prints a list of tasks.
+     * Returns a message for a list of tasks.
      */
-    public void printTasks(Tasks tasks) {
+    public String makeTasksMessage(Tasks tasks) {
         StringBuilder builder = new StringBuilder();
         for (int i = 1; i <= tasks.getSize(); i++) {
             builder.append(String.format("%d. %s\n", i, tasks.getUnchecked(i)));
         }
-        printMessage(builder.toString());
+        return builder.toString();
     }
 
     /**
-     * Prints a verification message for adding a task.
+     * Returns a verification message for adding a task.
      *
      * @param tasks   Set of tasks after adding newTask.
      * @param newTask The new task to be added.
      */
-    public void printAddedTask(Tasks tasks, Task newTask) {
-        printMessage(String.format("""
+    public String makeAddedTaskMessage(Tasks tasks, Task newTask) {
+        return String.format("""
                 Got it. I've added this task:
                   %s
-                Now you have %d tasks in the list.""", newTask, tasks.getSize()));
+                Now you have %d tasks in the list.""", newTask, tasks.getSize());
     }
 
     /**
-     * Prints a verification message for deleting a task.
+     * Returns a verification message for deleting a task.
      *
      * @param tasks   Set of tasks after deleting oldTask.
      * @param oldTask The old task to be deleted.
      */
-    public void printDeletedTask(Tasks tasks, Task oldTask) {
-        printMessage(String.format("""
+    public String makeDeletedTaskMessage(Tasks tasks, Task oldTask) {
+        return String.format("""
                 Noted. I've removed this task:
                   %s
-                Now you have %d tasks in the list.""", oldTask, tasks.getSize()));
+                Now you have %d tasks in the list.""", oldTask, tasks.getSize());
     }
 
     /**
-     * Prints a verification message for marking a task as done.
+     * Returns a verification message for marking a task as done.
      */
-    public void printMarkedTask(Task task) {
-        printMessage(String.format("Nice! I've marked this task as done:\n%s", task));
+    public String makeMarkedTaskMessage(Task task) {
+        return String.format("Nice! I've marked this task as done:\n%s", task);
     }
 
     /**
-     * Prints a verification message for marking a task as not done.
+     * Returns a verification message for marking a task as not done.
      */
-    public void printUnmarkedTask(Task task) {
-        printMessage(String.format("Ok, I've marked this task as not done yet:\n%s", task));
+    public String makeUnmarkedTaskMessage(Task task) {
+        return String.format("Ok, I've marked this task as not done yet:\n%s", task);
     }
 
     /**
-     * Formats the message nicely and prints it to stdout.
+     * Returns an error message.
      */
-    public void printMessage(String message) {
-        String leftPadding = " ".repeat(4);
-        String horizontalLine = "-".repeat(80);
-        System.out.println(leftPadding + horizontalLine);
-        for (String line : message.split("\n")) {
-            System.out.println(leftPadding + line);
-        }
-        System.out.println(leftPadding + horizontalLine);
-    }
-
-    /**
-     * Formats the error nicely and prints it to stdout.
-     */
-    public void printError(WaddlesException error) {
-        printMessage(String.format("ERROR:\n%s", error.getMessage()));
+    public String makeErrorMessage(WaddlesException error) {
+        return String.format("ERROR:\n%s", error.getMessage());
     }
 }
 
